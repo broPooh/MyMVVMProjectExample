cbf6cade17f014e088b32d136f6f31458ed701f8
broMac <yhs4705@naver.com>
2022-06-23 21:07:04 +0900

    DetailViewController의 뷰의 isHidden을 함수로 따로 뺴서 처리

--- MyMVVMProject/Detail/DetailViewController.swift
+++ MyMVVMProject/Detail/DetailViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class DetailViewController: BaseViewController {
-
+    
     private var detailView: DetailView!
     private var viewModel: BaseDetailViewModel!
 
@@ -40,16 +40,19 @@ final class DetailViewController: BaseViewController {
         
         viewModel.kakaoImage.bind { [weak self] kakaoImage in
             
-            self?.detailView.detailImageView.setImage(imageUrl: kakaoImage.imageURL)
-
-            self?.detailView.siteLable.isHidden = kakaoImage.displaySitename != nil ? false : true
-            self?.detailView.siteLable.text = kakaoImage.displaySitename ?? ""
+            guard let `self` = self else { return }
             
-            self?.detailView.dateLable.isHidden = kakaoImage.datetime != nil ? false : true
+            self.detailView.detailImageView.setImage(imageUrl: kakaoImage.imageURL)
+
+            //self?.detailView.siteLable.isHidden = kakaoImage.displaySitename != nil ? false : true
+            self.detailView.siteLable.isHidden = self.visibleCheck(text: kakaoImage.displaySitename)
+            self.detailView.siteLable.text = kakaoImage.displaySitename ?? ""
+            //self?.detailView.dateLable.isHidden = kakaoImage.datetime != nil ? false : true
+            self.detailView.siteLable.isHidden = self.visibleCheck(text: kakaoImage.displaySitename)
             
             if let dateTime = kakaoImage.datetime {
                 let date = dateTime.toDate
-                self?.detailView.dateLable.text = date.toString
+                self.detailView.dateLable.text = date.toString
             }
         }
 
@@ -61,6 +64,10 @@ final class DetailViewController: BaseViewController {
     
     @objc func dismissDetailView() {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func visibleCheck(text: String?) -> Bool {
+        return text != nil ? false : true
     }
     
 
